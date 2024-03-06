08a2fde7d116edeb6763b282fe2419d225a6e1de
hamzayaseenwd <hamzayaseen836@gmai.com>
2024-03-06 17:54:37 +0500

    Express JS

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const express = require("express");
 const app = express();
+const { todo } = require("./data");
+
+
+
+
+
+
+
+
+
+
 
 app.use(express.json());
 
 app.get("/task", (req, res) => {
   return res.json({
     message: "Hello world! using GET Method",
+    data: todo
   });
+  
 });
+
+
+
+app.get("/task/:id", (req, res) => {
+  
+  const id = req.params.id;
+  const find_todo = todo.find((elem) => {
+    return elem.id == id;
+  });
+
+  return res.json({ data: find_todo });
+});
+
+
 
 app.post("/task", (req, res) => {
   console.log(req.body);
